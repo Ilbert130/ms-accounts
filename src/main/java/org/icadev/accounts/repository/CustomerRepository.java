d3e39de1c3f56368fe6a58b72f540262e77fcd56
Ilbert130 <ilbertca27@gmail.com>
2025-09-03 22:20:51 -0400

    Repositories accounts and customer created

---
+++ src/main/java/org/icadev/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.icadev.accounts.repository;
+
+import org.icadev.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
