d5191b583c9c42b66e2b59754d92f93f042f4051
Ilbert130 <ilbertca27@gmail.com>
2025-09-04 14:50:53 -0400

    CustomerMapper created

---
+++ src/main/java/org/icadev/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package org.icadev.accounts.mapper;
+
+import org.icadev.accounts.dto.CustomerDto;
+import org.icadev.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
