62fc1ad027ee8cc7c8be759893b3e62db02b4291
Ilbert130 <ilbertca27@gmail.com>
2025-09-23 15:24:20 -0400

    Update configuration to a file system

--- src/main/java/org/icadev/accounts/dto/AccountsContactInfoDto.java
+++ src/main/java/org/icadev/accounts/dto/AccountsContactInfoDto.java
@@ -1,15 +1,20 @@
 package org.icadev.accounts.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+
+//We change record to class because we want to modify the values the properties in runtime execution
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
