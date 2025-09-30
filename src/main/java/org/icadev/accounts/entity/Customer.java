d4a5fe6012d0a75a5fd20497a464d115836a76f9
Ilbert130 <ilbertca27@gmail.com>
2025-09-30 15:03:47 -0400

    The last changes from section 7 and these too

--- src/main/java/org/icadev/accounts/entity/Customer.java
+++ src/main/java/org/icadev/accounts/entity/Customer.java
@@ -14,7 +14,7 @@ import org.hibernate.annotations.GenericGenerator;
 public class Customer extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
     private Long customerId;
 
